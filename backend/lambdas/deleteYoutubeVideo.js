b46cb56799f5e2e67ec062ddc112e119da12abb3
despinozap <despinozap.dev@gmail.com>
2022-09-01 20:24:04 -0400

    UPDATE: Lambda function for removing video improved with validations

--- backend/lambdas/deleteYoutubeVideo.js
+++ backend/lambdas/deleteYoutubeVideo.js
@@ -7,13 +7,41 @@ const documentClient = new AWS.DynamoDB.DocumentClient(
     }
 );
 
-// Function to delete video in DB
-const deleteYoutubeVideo = async ({ id }) => {
+// Function to get a video by ID from DB
+const getYoutubeVideo = async ({ id }) => {
     
     const params = {
         TableName: 'videos',
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues: {
+            ":id": id
+        }
+    };
+    
+    try
+    {
+        const data = await documentClient.query(params)
+        .promise();
+     
+        // Get video if exists
+        const video = (data.Count > 0) ? data.Items[0] : null;
+        
+        return video;
+    }
+    catch(ex)
+    {
+        return null;
+    }
+}
+
+// Function to delete video in DB
+const deleteYoutubeVideo = async ({ id, created }) => {
+
+    const params = {
+        TableName: 'videos',
         Key: {
-            id: id
+            id: id,
+            created: created
         }
     };
     
@@ -50,29 +78,52 @@ exports.handler = async (event) => {
             )
         };
     }
-    // All validations passed
     else
     {
-        const deleted = await deleteYoutubeVideo(
+        const video = await getYoutubeVideo(
             {
                 id: event.id
             }
         );
         
-        if(deleted)
+        // If video exists
+        if(video !== null)
         {
-            response = {
-                statusCode: 200
-            };
+            const deleted = await deleteYoutubeVideo(
+                {
+                    id: video.id,
+                    created: video.created
+                }
+            );
+        
+            if(deleted)
+            {
+                response = {
+                    statusCode: 200
+                };
+            }
+            else
+            {
+                response = {
+                    statusCode: 500,
+                    body: JSON.stringify(
+                        {
+                            error: {
+                                message: "Fail on deleting video."        
+                            }
+                        }
+                    )
+                };
+            }
         }
         else
         {
             response = {
-                statusCode: 500,
+                statusCode: 400,
                 body: JSON.stringify(
                     {
                         error: {
-                            message: "Fail on deleting video."        
+                            message: "Video not found."        
                         }
                     }
                 )
